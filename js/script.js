10171086dc2cb1d356150c83690574a3fe8c13a8
othensa <othensa@gmail.com>
2024-10-21 23:09:39 +0700

    update

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ document.getElementById("nickname").innerHTML = nickname
 document.getElementById("fillnickname").addEventListener("click", function() {
     replaceName();
 }
+
+function validateForm {
+    const name = document.form["message-form]["name"].value
+    const name = document.form["message-form]["email"].value
+    const name = document.form["message-form]["message"].value
+}
+
+document.getElementById("name"). innerHTML = name;
+document.getElementById("email"). innerHTML = email;
+document.getElementById("message"). innerHTML = message;
+
+return false;
